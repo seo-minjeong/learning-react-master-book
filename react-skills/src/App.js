bce38f0c5b120e44cf619edc415905d5b54d3173
seominjeong <smj12938@gmail.com>
2022-07-18 23:51:27 +0900

    redux todo component

--- react-skills/src/App.js
+++ react-skills/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Counter from "./Redux/components/Counter";
+import Todos from "./Redux/components/Todos";
 
 const App = () => {
   return (
     <div>
       <Counter number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 };
